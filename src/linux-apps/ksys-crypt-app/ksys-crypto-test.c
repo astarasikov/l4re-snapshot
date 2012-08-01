77a59d5e0f3d9c6b537cb9f111c989c94582227c
Alexander Tarasikov <alexander.tarasikov@gmail.com>
2012-08-01 15:31:12 +0400

    Fix the demo application for homework 3

--- src/linux-apps/ksys-crypt-app/ksys-crypto-test.c
+++ src/linux-apps/ksys-crypt-app/ksys-crypto-test.c
@@ -8,8 +8,8 @@
 
 #include "ksys_ss_l4_crypto.h"
 
-#define IV_SIZE MAX_IV_SIZE
-#define KEY_SIZE MAX_KEY_SIZE
+#define IV_SIZE 16
+#define KEY_SIZE 16
 #define BUF_SIZE 64
 
 #define MSG "hello world 12345!"
@@ -38,16 +38,24 @@ static void hexdump(char *in, size_t count) {
 
 int main() {
 	int rc = 0;
-	static struct ksys_crypto_request req = {};
 	char iv[IV_SIZE] = {};
 	char key[KEY_SIZE] = {};
-	char raw[BUF_SIZE];
+	char raw[BUF_SIZE] = {};
 	
 	puts("KSYS crypto test");
 	memset(iv, 0xab, IV_SIZE);
 	memset(key, 0x13, KEY_SIZE);
 	memset(raw, 0, BUF_SIZE);
 	memcpy(raw, MSG, sizeof(MSG));
+	
+	static struct ksys_crypto_request req = {
+		.size = BUF_SIZE,
+		.iv_size = IV_SIZE,
+		.key_size = KEY_SIZE,
+	};
+	req.iv = iv;
+	req.key = key;
+	req.data = raw;
 
 	int fd = open("/dev/l4_crypto", O_RDWR);
 	if (fd < 0) {
@@ -72,6 +80,8 @@ done:
 		close(fd);
 	}
 
+	//spin forever because we're called
+	//instead of init and don't want a kernel panic
 	while (1) {}
 
 	return rc;
